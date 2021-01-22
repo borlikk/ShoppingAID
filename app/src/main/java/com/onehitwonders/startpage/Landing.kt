61caeb0c01be930938dd7d70c4742ea39a76311e
borlikk <theflyingpixel@hotmail.com>
2021-01-22 18:00:46 +0000

    gnvotprei

--- app/src/main/java/com/onehitwonders/startpage/Landing.kt
+++ app/src/main/java/com/onehitwonders/startpage/Landing.kt
@@ -39,7 +39,7 @@ class Landing : AppCompatActivity() {
         val dao = ShoppingDatabase.getInstance(this).shoppingDao
 
 
-        /**
+    /**
         val shoppings = listOf(
             Shopping(0, "GaiaShopping", "Gaia", "10:00 às 22:30", 600, "https://www.gaiashopping.pt/mapa"),
             Shopping(0, "ArrabidaShopping", "Arrabida", "10:00 às 22:30", 1000, "https://www.arrabidashopping.com/mapa")
@@ -49,6 +49,7 @@ class Landing : AppCompatActivity() {
             Loja("Sport Zone", 1, 0, 15, "https://www.sportzone.pt", "Loja de produtos de Desporto", "Seg a Qua: 10:00 às 22:00 Dom, Sáb: 10:00 às 13:00 Qui: 10:00 às 19:00"),
             Loja("FNAC", 1, 1, 15, "http://www.fnac.pt", "Com 360.000 referências de artigos, entre as quais livros (160.000), música (119.000), filmes (15.000), som e imagem (10.000), telecomunicações e informática (10.000) e jogos (12.000), a Fnac dispõe de uma oferta inigualável em produtos culturais e tecnológicos. Além da escolha de produtos, a Fnac oferece inúmeros serviços como a encomenda de livros e discos, as entregas ao domicílio ou o serviço pós-venda. No apoio à Fnac dispõe ainda de forum e espaço fotográfico.", "Seg a Sex: 10:00 às 22:30 Dom, Sáb: 10:00 às 13:00")
         )
+
 
         val categorias = listOf(
             Categoria(0, "Sport Zone", "Desporto"),
@@ -86,12 +87,22 @@ class Landing : AppCompatActivity() {
             outdoor.forEach { dao.insertOutdoor(it) }
             indoor.forEach { dao.insertIndoor(it) }
         }
-        **/
 
+        **/
         val images = listOf(
             R.drawable.ass,
             R.drawable.monke
         )
+        val lojas = listOf(
+            Loja("H&M", 2, 0, 15, "https://www.sportzone.pt", "Loja de produtos de Desporto", "Seg a Qua: 10:00 às 22:00 Dom, Sáb: 10:00 às 13:00 Qui: 10:00 às 19:00"),
+            Loja("Burger King", 2, 1, 15, "http://www.fnac.pt", "Com 360.000 referências de artigos, entre as quais livros (160.000), música (119.000), filmes (15.000), som e imagem (10.000), telecomunicações e informática (10.000) e jogos (12.000), a Fnac dispõe de uma oferta inigualável em produtos culturais e tecnológicos. Além da escolha de produtos, a Fnac oferece inúmeros serviços como a encomenda de livros e discos, as entregas ao domicílio ou o serviço pós-venda. No apoio à Fnac dispõe ainda de forum e espaço fotográfico.", "Seg a Sex: 10:00 às 22:30 Dom, Sáb: 10:00 às 13:00")
+        )
+
+        lifecycleScope.launch{
+
+            lojas.forEach { dao.insertLoja(it) }
+
+        }
 
         setContentView(R.layout.landing)
 
